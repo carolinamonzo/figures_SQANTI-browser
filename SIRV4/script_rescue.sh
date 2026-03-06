7e59152fe21b6dd5f2e97946e03e2f53b2a8fb9d
Carolina  Monzó <carolmonzoc@gmail.com>
2026-03-06 11:49:08 +0100

    updating filter and qc isoseq

--- SIRV4/script_rescue.sh
+++ SIRV4/script_rescue.sh
@@ -5,8 +5,7 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=20gb
 #SBATCH --qos=short
-#SBATCH --time=2:00:00
-
+#SBATCH --time=4:00:00
 #SBATCH --mail-type=BEGIN,END,FAIL #Send e-mails
 #SBATCH --mail-user=carolina.monzo@csic.es
 
@@ -15,6 +14,8 @@ module load anaconda
 conda activate sqanti3
 module swap gnu9 gnu14/14.2.0
 
-#python ../../software/SQANTI3/sqanti3_qc.py --isoforms /storage/gge/genomes/mouse_ref_NIH/reference_genome/mm39.ncbiRefSeq_SIRV.gtf --refGTF /storage/gge/genomes/mouse_ref_NIH/reference_genome/mm39.ncbiRefSeq_SIRV.gtf --refFasta /storage/gge/genomes/mouse_ref_NIH/reference_genome/mm39_SIRV.fa --CAGE_peak /storage/gge/genomes/mouse_ref_NIH/reference_genome/lft_mm39_CAGE.bed --polyA_motif_list mouse_and_human.polyA_motif.txt --polyA_peak mouse_polyA_site.bed -c /storage/gge/nih/Illumina_short_reads/short_reads/NOVOGENE_stranded/mapped/B31/B31_EKRN230014690-1A_HFHGVDSX7_L4SJ.out.tab --SR_bam /storage/gge/nih/Illumina_short_reads/short_reads/NOVOGENE_stranded/mapped/B31/B31_EKRN230014690-1A_HFHGVDSX7_L4Aligned.sortedByCoord.out.bam -o reference_QC -d reference_QC --skipORF
+python ../../software/SQANTI3/sqanti3_qc.py --isoforms /storage/gge/genomes/mouse_ref_NIH/reference_genome/mm39.ncbiRefSeq_SIRV.gtf --refGTF /storage/gge/genomes/mouse_ref_NIH/reference_genome/mm39.ncbiRefSeq_SIRV.gtf --refFasta /storage/gge/genomes/mouse_ref_NIH/reference_genome/mm39_SIRV.fa --CAGE_peak /storage/gge/genomes/mouse_ref_NIH/reference_genome/lft_mm39_CAGE.bed --polyA_motif_list mouse_and_human.polyA_motif.txt --polyA_peak mouse_polyA_site.bed -c /storage/gge/nih/Illumina_short_reads/short_reads/NOVOGENE_stranded/mapped/B31/B31_EKRN230014690-1A_HFHGVDSX7_L4SJ.out.tab --SR_bam /storage/gge/nih/Illumina_short_reads/short_reads/NOVOGENE_stranded/mapped/B31/B31_EKRN230014690-1A_HFHGVDSX7_L4Aligned.sortedByCoord.out.bam -o reference_QC -d reference_QC --skipORF
 
-python ../../software/SQANTI3/sqanti3_rescue.py ml --filter_class collapse_filter/B31_MLresult_classification.txt --refGTF /storage/gge/genomes/mouse_ref_NIH/reference_genome/mm39.ncbiRefSeq_SIRV.gtf --refFasta /storage/gge/genomes/mouse_ref_NIH/reference_genome/mm39_SIRV.fa --rescue_gtf collapse_QC/B31_corrected.gtf --rescue_isoforms collapse_QC/B31.fasta --refClassif reference_QC/reference_QC_classification.txt --mode full --random_forest collapse_filter/randomforest.RData -o collapse_rescue -d collapse_rescue  
+python ../../software/SQANTI3/sqanti3_rescue.py ml --filter_class collapse_filter/B31_MLresult_classification.txt --refGTF /storage/gge/genomes/mouse_ref_NIH/reference_genome/mm39.ncbiRefSeq_SIRV.gtf --refFasta /storage/gge/genomes/mouse_ref_NIH/reference_genome/mm39_SIRV.fa --rescue_gtf collapse_filter/B31_filtered.gtf --rescue_isoforms collapse_QC/B31.fasta --refClassif reference_QC/reference_QC_classification.txt --mode full --random_forest collapse_filter/randomforest.RData -o collapse_rescue -d collapse_rescue
+
+python ../../software/SQANTI3/sqanti3_qc.py --isoforms collapse_rescue/B31_rescued.gtf --refGTF /storage/gge/genomes/mouse_ref_NIH/reference_genome/mm39.ncbiRefSeq_SIRV.gtf --refFasta /storage/gge/genomes/mouse_ref_NIH/reference_genome/mm39_SIRV.fa --CAGE_peak /storage/gge/genomes/mouse_ref_NIH/reference_genome/lft_mm39_CAGE.bed --polyA_motif_list mouse_and_human.polyA_motif.txt --polyA_peak mouse_polyA_site.bed -c /storage/gge/nih/Illumina_short_reads/short_reads/NOVOGENE_stranded/mapped/B31/B31_EKRN230014690-1A_HFHGVDSX7_L4SJ.out.tab --SR_bam /storage/gge/nih/Illumina_short_reads/short_reads/NOVOGENE_stranded/mapped/B31/B31_EKRN230014690-1A_HFHGVDSX7_L4Aligned.sortedByCoord.out.bam -o isoseq_rescue -d collapse_rescue/QC --skipORF
--- SIRV4/script_sqanti_browser.sh
+++ SIRV4/script_sqanti_browser.sh
@@ -1,11 +1,11 @@
-for file in $(cat gtfs.fof);
-do filename="${file%.*}";
-outname="${filename%_S*}";
-python ~/workspace/SQANTI-browser/sqanti_browser.py --gtf ${filename}/${filename}_corrected.gtf --classification ${filename}/${filename}_classification.txt --output ./${filename} --genome SIRV4 --tables --no-category-tracks --hub-name ${outname} --twobit ~/workspace/SQANTI-browser/example/SQANTI3_QC_custom_genome/SIRVS.2bit;
-done
+#for file in $(cat gtfs.fof);
+#do filename="${file%.*}";
+#outname="${filename%_S*}";
+#python ~/workspace/SQANTI-browser/sqanti_browser.py --gtf ${filename}/${filename}_corrected.gtf --classification ${filename}/${filename}_classification.txt --output ./${filename} --genome SIRV4 --tables --no-category-tracks --hub-name ${outname} --twobit ~/workspace/SQANTI-browser/example/SQANTI3_QC_custom_genome/SIRVS.2bit;
+#done
 
-python ~/workspace/SQANTI-browser/sqanti_browser.py --gtf collapse_QC/PacBio_isoforms_collapse_QC.gtf --classification collapse_QC/PacBio_isoforms_collapse_QC_classification.txt --output ./collapse_QC --genome SIRV4 --tables --no-category-tracks --hub-name PacBio_isoseq_collapse_QC --twobit ~/workspace/SQANTI-browser/example/SQANTI3_QC_custom_genome/SIRVS.2bit
+python ~/workspace/SQANTI-browser/sqanti_browser.py --gtf collapse_QC/PacBio_isoforms_collapse_QC.gtf --classification collapse_QC/PacBio_isoforms_collapse_QC_classification.txt --output ./collapse_QC --genome SIRV4 --tables --no-category-tracks --hub-name PacBio_isoseq_collapse_QC --twobit ~/workspace/SQANTI-browser/example/SQANTI3_QC_custom_genome/SIRVS.2bit --no-highlight
 
-python ~/workspace/SQANTI-browser/sqanti_browser.py --gtf collapse_filter/PacBio_isoseq_collapse_filter.gtf --classification collapse_filter/PacBio_isoseq_collapse_filter_classification.txt --output ./collapse_filter --genome SIRV4 --tables --no-category-tracks --hub-name PacBio_isoseq_collapse_filter --twobit ~/workspace/SQANTI-browser/example/SQANTI3_QC_custom_genome/SIRVS.2bit
+python ~/workspace/SQANTI-browser/sqanti_browser.py --gtf collapse_filter/PacBio_isoseq_collapse_filter.gtf --classification collapse_filter/PacBio_isoseq_collapse_filter_classification.txt --output ./collapse_filter --genome SIRV4 --tables --no-category-tracks --hub-name PacBio_isoseq_collapse_filter --twobit ~/workspace/SQANTI-browser/example/SQANTI3_QC_custom_genome/SIRVS.2bit --no-highlight
 
-python ~/workspace/SQANTI-browser/sqanti_browser.py --gtf collapse_rescue/PacBio_isoseq_collapse_rescue.gtf --classification collapse_rescue/PacBio_isoseq_collapse_rescue_classification.txt --output ./collapse_rescue --genome SIRV4 --tables --no-category-tracks --hub-name PacBio_isoseq_collapse_rescue --twobit ~/workspace/SQANTI-browser/example/SQANTI3_QC_custom_genome/SIRVS.2bit
+python ~/workspace/SQANTI-browser/sqanti_browser.py --gtf collapse_rescue/PacBio_isoseq_collapse_rescue.gtf --classification collapse_rescue/PacBio_isoseq_collapse_rescue_classification.txt --output ./collapse_rescue --genome SIRV4 --tables --no-category-tracks --hub-name PacBio_isoseq_collapse_rescue --twobit ~/workspace/SQANTI-browser/example/SQANTI3_QC_custom_genome/SIRVS.2bit --no-highlight
